df127c0487bedaf247946d094be5f3a4308aaf3b
kubavation <kubavation@wp.pl>
2019-11-02 12:45:25 +0100

    chapter 3 - String reverse completed tests

--- chapter_3_11/src/test/java/string_reverse/StringReverseTest.java
+++ chapter_3_11/src/test/java/string_reverse/StringReverseTest.java
@@ -1,13 +1,51 @@
 package string_reverse;
 
-import org.junit.jupiter.api.Test;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.junit.runners.Parameterized;
+import org.junit.runners.parameterized.BlockJUnit4ClassRunnerWithParameters;
 
-import static org.junit.jupiter.api.Assertions.*;
+@RunWith(JUnitParamsRunner.class)
+public class StringReverseTest {
 
+    public static final Object[] getValidInputs() {
+        return new Object[][] {
+                {"ala ma kota", "atok am ala"},
+                {"kot ma ale", "ela am tok"},
+                {"test","tset"}
+        };
+    }
 
-class StringReverseTest {
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNullPointerExceptionIfNullInput  () {
+        String nullStr = null;
+
+        StringReverse.reverse(nullStr);
+    }
 
     @Test
-    void reverse() {
+    @Parameters(method = "getValidInputs")
+    public void shouldReturnReversedStringIfValidInput(String input, String expected) {
+        String reversed = StringReverse.reverse(input);
+
+        Assert.assertEquals(reversed, expected);
     }
+
+    @Test
+    public void shouldReturnEmptyStringIfEmptyInput() {
+        String emptyInput = "";
+
+        Assert.assertEquals(emptyInput, StringReverse.reverse(emptyInput));
+    }
+
+
+
+
+
+
+
 }
