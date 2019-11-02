3709af74708dc699b24a76388596453c8c2b7e4e
kubavation <kubavation@wp.pl>
2019-11-02 12:58:20 +0100

    completed hashmap tests

--- chapter_3_11/src/test/java/hashmap/HashMapTest.java
+++ chapter_3_11/src/test/java/hashmap/HashMapTest.java
@@ -1,11 +1,65 @@
 package hashmap;
 
 import junitparams.JUnitParamsRunner;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
 
 @RunWith(JUnitParamsRunner.class)
 public class HashMapTest {
 
+    private HashMap<String, String> hashMap;
 
-    
+    @Before
+    public void init() {
+        hashMap = new HashMap<String, String>();
+    }
+
+    @Test
+    public void shouldPutMethodInsertNewValue() {
+        String value = "value";
+        String key = "key";
+
+        hashMap.put(key,value);
+
+        assertEquals(value, hashMap.get(key));
+    }
+
+    @Test
+    public void shouldRecordWithSameKeyOverwriteOldValue() {
+        String oldValue = "oldValue";
+        String newValue = "newValue";
+        String key = "key";
+
+        hashMap.put(key, oldValue);
+        hashMap.put(key, newValue);
+
+        assertEquals(newValue, hashMap.get(key));
+    }
+
+
+
+    @Test
+    public void shouldClearMethodRemoveAllContent() {
+        hashMap.put("1","1");
+        hashMap.put("2","2");
+
+        hashMap.clear();
+
+        assertEquals(0, hashMap.size());
+    }
+
+    @Test
+    public void shouldNullKeyBeInsertedToHashMap() {
+        String nullKey = null;
+
+        hashMap.put(nullKey, "test");
+
+        assertEquals("test", hashMap.get(nullKey));
+    }
 }
